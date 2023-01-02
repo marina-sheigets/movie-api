5bb06882e097d8a68ef474ae230db4956d18f81b
Marina Sheigets <marinasejgec34@gmail.com>
2023-01-02 23:17:02 +0200

    Create Rest API Models

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.log(err));
 app.listen(8800, () => {
 	console.log('Server is running ...');
 });
