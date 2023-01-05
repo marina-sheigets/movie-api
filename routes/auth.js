8f6801a24e6c51b81474329fbf3f9ea99919a54b
Marina Sheigets <marinasejgec34@gmail.com>
2023-01-05 21:59:33 +0200

    feat:create  auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
 // REGISTER
 router.post('/register', async (req, res) => {
+	const user = User.findOne({ email: req.body.email });
+	if (user) {
+		res.status(403).json('User already exists !');
+	}
+
 	const hashedPassword = CryptoJS.AES.encrypt(
 		req.body.password,
-		proccess.env.SECRET_KEY
+		process.env.SECRET_KEY
 	).toString();
 	const newUser = await new User({
 		username: req.body.username,
@@ -17,6 +25,34 @@ router.post('/register', async (req, res) => {
 		res.status(201).json(user);
 	} catch (e) {
 		res.status(500).json(e);
+	}
+});
+
+//LOGIN
+
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		if (!user) {
+			res.status(401).json('Wrong password or username !');
+		}
+		const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+		const initialPassword = bytes.toString(CryptoJS.enc.Utf8);
+		if (initialPassword !== req.body.password) {
+			res.status(401).json('Wrong password or username !');
+		}
+		const accessToken = jwt.sign(
+			{ id: user._id, isAdmin: user.isAdmin },
+			process.env.SECRET_KEY,
+			{
+				expiresIn: '1h',
+			}
+		);
+
+		const { password, ...info } = user._doc;
+		res.status(200).json({ ...info, accessToken });
+	} catch (err) {
+		res.status(500).json(err);
 	}
 });
 
